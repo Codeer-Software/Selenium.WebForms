889c750af363b3f72f75a1b5c6fe78520a65c44b
Bin Matsui <matsui-bin@codeer.co.jp>
2016-03-18 13:45:40 +0900

    Refactoring

--- Project/Selenium.WebForms/Properties/AssemblyInfo.cs
+++ Project/Selenium.WebForms/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("9cce6a26-5341-4cae-a3c2-491fd7ba640b")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.0.2.0")]
+[assembly: AssemblyFileVersion("0.0.2.0")]
